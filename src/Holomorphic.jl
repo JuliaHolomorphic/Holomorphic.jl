1fb04543cf94515281a09129e12b7b86c643751a
Sheehan Olver <solver@mac.com>
2015-12-20 15:36:20 +1100

    started strip routines

--- src/Holomorphic.jl
+++ src/Holomorphic.jl
@@ -3,16 +3,23 @@ module Holomorphic
     using Base, ApproxFun, SingularIntegralEquations
 
 
-import ApproxFun: UnivariateSpace, domain, evaluate, ComplexBasis, spacescompatible
+import ApproxFun: UnivariateSpace, domain, evaluate, ComplexBasis, spacescompatible,
+                    Space, defaultFun
 import SingularIntegralEquations: stieltjes
 
 # represents the whole Planess
 immutable ComplexPlane <: Domain{Complex128,2} end
+
+Base.intersect(a::ComplexPlane,b::ComplexPlane)=a
+
+
 # represents S\A
 immutable Complement{SS,AA,T,d} <: Domain{T,d}
     full::SS
     del::AA
 end
+
+Base.intersect(a::Complement,b::Complement)=Complement(a.full ∩ b.full,a.del ∪ b.del)
 
 Complement(A::Domain,B::Domain)=Complement{typeof(A),typeof(B),eltype(A),ndims(A)}(A,B)
 Base.setdiff(a::Domain,b::Domain)=Complement(a,b)
@@ -36,4 +43,26 @@ evaluate(v::AbstractVector,sp::StieltjesSpace,z)=stieltjes(sp.space,v,z)
 
 stieltjes(f::Fun)=Fun(f.coefficients,StieltjesSpace(space(f)))
 
+# construct spaces for complement
+# we assume boundedness
+
+Space{DD<:Interval}(Γ::Complement{ComplexPlane,DD})=StieltjesSpace(JacobiWeight(0.5,0.5,Γ.del))
+Space{DD<:UnionDomain}(Γ::Complement{ComplexPlane,DD})=mapreduce(d->StieltjesSpace(d).space,PiecewiseSpace,Γ.del.domains)
+
+
+
+
+# represents strip between a < im z < b
+# TODO: rotations
+immutable Strip <: Domain{Complex128,2}
+    a::Float64
+    b::Float64
+end
+
+
+
+
 end #module
+
+
+
