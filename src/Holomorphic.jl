d98abe6c1de2ac554afef277fcf4cb6f31a48c25
Sheehan Olver <solver@mac.com>
2015-12-20 14:39:29 +1100

    Started Stieltjes space

---
+++ src/Holomorphic.jl
@@ -0,0 +1,39 @@
+__precompile__()
+module Holomorphic
+    using Base, ApproxFun, SingularIntegralEquations
+
+
+import ApproxFun: UnivariateSpace, domain, evaluate, ComplexBasis, spacescompatible
+import SingularIntegralEquations: stieltjes
+
+# represents the whole Planess
+immutable ComplexPlane <: Domain{Complex128,2} end
+# represents S\A
+immutable Complement{SS,AA,T,d} <: Domain{T,d}
+    full::SS
+    del::AA
+end
+
+Complement(A::Domain,B::Domain)=Complement{typeof(A),typeof(B),eltype(A),ndims(A)}(A,B)
+Base.setdiff(a::Domain,b::Domain)=Complement(a,b)
+
+
+immutable StieltjesSpace{S,DD} <:  Space{ComplexBasis,Complement{ComplexPlane,DD},2}
+    space::S
+    function StieltjesSpace(sp::S)
+        @assert isa(domain(sp),DD)
+        new(sp)
+    end
+end
+
+StieltjesSpace(sp::UnivariateSpace)=StieltjesSpace{typeof(sp),typeof(domain(sp))}(sp)
+spacescompatible(a::StieltjesSpace,b::StieltjesSpace)=spacescompatible(a.space,b.space)
+
+domain(sp::StieltjesSpace)=Complement(ComplexPlane(),domain(sp.space))
+
+
+evaluate(sp::StieltjesSpace,v::Vector,z)=stieltjes(sp.space,v,z)
+
+stieltjes(f::Fun)=Fun(f.coefficients,StieltjesSpace(space(f)))
+
+end #module
