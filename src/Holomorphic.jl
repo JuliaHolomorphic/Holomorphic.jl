1a632af02aab8ae551a84150a4d0774124457919
Sheehan Olver <solver@mac.com>
2015-12-27 14:39:55 +1100

    First constructor works!  f=Fun(z->z-sqrt(1+z)*sqrt(-1+z)+3,ℂ\𝕀)

--- src/Holomorphic.jl
+++ src/Holomorphic.jl
@@ -4,11 +4,17 @@ module Holomorphic
 
 
 import ApproxFun: UnivariateSpace, domain, evaluate, ComplexBasis, spacescompatible,
-                    Space, defaultFun, union_rule, ConstantSpace
+                    Space, defaultFun, union_rule, ConstantSpace, points, checkpoints,
+                    transform, plan_transform
 import SingularIntegralEquations: stieltjes
+
+
+export ℂ
 
 # represents the whole Planess
 immutable ComplexPlane <: Domain{Complex128,2} end
+
+const ℂ=ComplexPlane()
 
 Base.intersect(a::ComplexPlane,b::ComplexPlane)=a
 
@@ -36,7 +42,7 @@ end
 StieltjesSpace(sp::UnivariateSpace)=StieltjesSpace{typeof(sp),typeof(domain(sp))}(sp)
 spacescompatible(a::StieltjesSpace,b::StieltjesSpace)=spacescompatible(a.space,b.space)
 
-domain(sp::StieltjesSpace)=ComplexPlane()\domain(sp.space)
+domain(sp::StieltjesSpace)=ℂ\domain(sp.space)
 evaluate(v::AbstractVector,sp::StieltjesSpace,z)=v[1]+stieltjes(sp.space,v[2:end],z)
 stieltjes(f::Fun)=Fun([0;f.coefficients],StieltjesSpace(space(f)))
 
@@ -49,9 +55,37 @@ Base.ones(S::StieltjesSpace)=Fun(ones(1),S)
 # construct spaces for complement
 # we assume boundedness
 
-Space{DD<:Interval}(Γ::Complement{ComplexPlane,DD})=StieltjesSpace(JacobiWeight(0.5,0.5,Γ.del))
+Space{DD<:Interval}(Γ::Complement{ComplexPlane,DD})=StieltjesSpace(JacobiWeight(0.5,0.5,Ultraspherical{1}(Γ.del)))
 Space{DD<:Circle}(Γ::Complement{ComplexPlane,DD})=StieltjesSpace(Laurent(Γ.del))
 Space{DD<:UnionDomain}(Γ::Complement{ComplexPlane,DD})=StieltjesSpace(PiecewiseSpace(map(d->Space(d).space,map(x->ComplexPlane()\x,Γ.del))))
+
+
+
+## Interval
+
+checkpoints{DD<:Interval}(C::Complement{ComplexPlane,DD})=[fromcanonical(C.del,1im),fromcanonical(C.del,2.)]
+
+joukowsky(z)=(z+1./z)./2
+
+points{DD<:Interval}(S::StieltjesSpace{JacobiWeight{Ultraspherical{1,DD},DD},DD},n)=
+    fromcanonical(domain(S).del,joukowsky(points((1-1/n)*Circle(),n)))
+
+plan_transform{DD<:Interval}(S::StieltjesSpace{JacobiWeight{Ultraspherical{1,DD},DD},DD},vals::Vector)=
+        plan_transform(Taylor((1-1/length(vals))*Circle()),vals)
+
+function transform{DD<:Interval}(S::StieltjesSpace{JacobiWeight{Ultraspherical{1,DD},DD},DD},vals,plan)
+    @assert S.space.α==S.space.β==0.5
+    n=length(vals)
+    cfs=transform(Taylor((1-1/n)*Circle()),vals,plan)
+    for k=2:length(cfs)
+        cfs[k]*=1/((1-1/n)^(k-1)*π)
+    end
+    cfs
+end
+
+
+## ℂ\Circle()
+
 
 
 
@@ -73,6 +107,7 @@ Base.show(io::IO,d::Complement)=print(io,"$(d.full) \\$(d.del)")
 
 
 end #module
+
 
 
 
